3affa867965525872aa89b379246a59ddeb1a6af
Aarthi Ramakrishnan <aarthi0223@gmail.com>
2018-03-09 10:34:19 -0500

    Update run_snakemake_cluster.sh

--- run_snakemake_cluster.sh
+++ run_snakemake_cluster.sh
@@ -1,3 +1,17 @@
 source activate rnaseq_charmer
-snakemake --snakefile Snakefile -j 499 --cluster-config cluster.json --cluster "bsub -m {cluster.group} -P {cluster.allocation} -q {cluster.queue} -n {cluster.tasks} -R {cluster.resources} -W {cluster.walltime} -J {cluster.jobname} -o {cluster.output} -e {cluster.error}"
+
+snakemake --snakefile Snakefile \
+    -j 499 \
+    --cluster-config cluster.json \
+    --cluster "bsub \
+    -m {cluster.group} \
+    -P {cluster.allocation} \
+    -q {cluster.queue} \
+    -n {cluster.tasks} \
+    -R {cluster.resources} \
+    -W {cluster.walltime} \
+    -J {cluster.jobname} \
+    -o {cluster.output} \
+    -e {cluster.error}"
+
 source deactivate
